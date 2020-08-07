1fb099545948c6a3cb6e46c641e8d2d4a30c17d0
Rizal Satria Kamal <rizalsatriakamal@gmail.com>
2020-08-07 20:05:14 +0700

    Apdet buku di Komponen Orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,111 +1,108 @@
-const { Op } = require('sequelize');
-const {
-    orders,
-    order_detail,
-    users
-} = require('../model');
+const { Op } = require("sequelize");
+const { orders, order_detail, users, buku } = require("../model");
 
 const create = async (req, res) => {
-    try {
-        const params = (req.body);
-        const data = await orders.create(params, {
-            include: [
-                {
-                    model: order_detail,
-                    as: 'order_detail'
-                }
-            ]
-        });
-        return res.status(200).send({
-            message: 'OK',
-            data,
-        });
-    } catch (err) {
-        return res.status(400).send({
-            message: err.message,
-        })
-    }
-}
+  try {
+    const params = req.body;
+    const data = await orders.create(params, {
+      include: [
+        {
+          model: order_detail,
+          as: "order_detail",
+        },
+      ],
+    });
+    return res.status(200).send({
+      message: "OK",
+      data,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
+};
 
 const get_by_id = async (req, res) => {
-    try {
-        const data = await orders.findByPk(req.params.id,
-            {
-                include: [
-                    {
-                        model: users,
-                        as: 'customers_detail'
-                    },
-                    {
-                        model: order_detail,
-                        as: 'order_detail'
-                    }
-                ]
-            });
-        if (!data) {
-            return res.status(400).send({
-                message: 'ID tidak ditemukan',
-            })
-        }
-        return res.status(200).send({
-            message: 'OK',
-            data,
-        });
-    } catch (err) {
-        return res.status(400).send({
-            message: err.message,
-        })
+  try {
+    const data = await orders.findByPk(req.params.id, {
+      include: [
+        {
+          model: users,
+          as: "customers_detail",
+        },
+        {
+          model: order_detail,
+          as: "order_detail",
+        },
+        { model: buku, as: "buku" },
+      ],
+    });
+    if (!data) {
+      return res.status(400).send({
+        message: "ID tidak ditemukan",
+      });
     }
-}
+    return res.status(200).send({
+      message: "OK",
+      data,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
+};
 
 const get_list = async (req, res) => {
-    try {
-        const params = (req.query);
-        const query = {
-            order: [
-                ['id', 'DESC']
-            ],
-            where: {},
-            limit: 10,
-            offset: 0,
-            attributes: this.attributes,
-            distinct: true,
-            include: [
-                {
-                    model: users,
-                    as: 'customers_detail'
-                },
-                {
-                    model: order_detail,
-                    as: 'order_detail',
-                }
-            ]
-        };
-        if (params.title) query.where.user_id = {
-            [Op.like]: `%${params.user_id}%`,
-        };
-        if (params.sort_by && params.sort_type) query.order =
-            [[params.sort_by, params.sort_type]];
-        if (params.limit) query.limit = Number(params.limit);
-        if (params.page) query.offset = Number(query.limit) *
-            ((Number(params.page || 1) || 1) - 1);
-        const data = await orders.findAndCountAll(query);
-        data.limit = query.limit;
-        data.offset = query.offset;
-        data.page = (query.offset / query.limit) + 1;
-        return res.status(200).send({
-            message: 'OK',
-            data,
-        });
-    } catch (err) {
-        return res.status(400).send({
-            message: err.message,
-        })
-    }
-}
+  try {
+    const params = req.query;
+    const query = {
+      order: [["id", "DESC"]],
+      where: {},
+      limit: 10,
+      offset: 0,
+      attributes: this.attributes,
+      distinct: true,
+      include: [
+        {
+          model: users,
+          as: "customers_detail",
+        },
+        {
+          model: order_detail,
+          as: "order_detail",
+        },
+        { model: buku, as: "buku" },
+      ],
+    };
+    if (params.title)
+      query.where.user_id = {
+        [Op.like]: `%${params.user_id}%`,
+      };
+    if (params.sort_by && params.sort_type)
+      query.order = [[params.sort_by, params.sort_type]];
+    if (params.limit) query.limit = Number(params.limit);
+    if (params.page)
+      query.offset =
+        Number(query.limit) * ((Number(params.page || 1) || 1) - 1);
+    const data = await orders.findAndCountAll(query);
+    data.limit = query.limit;
+    data.offset = query.offset;
+    data.page = query.offset / query.limit + 1;
+    return res.status(200).send({
+      message: "OK",
+      data,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
+};
 
 module.exports = {
-    create,
-    get_by_id,
-    get_list,
-}
+  create,
+  get_by_id,
+  get_list,
+};
