11bede9b6f8579380934451c5043d6550ffcf34c
Rizal Satria Kamal <rizalsatriakamal@gmail.com>
2020-08-09 12:42:12 +0700

    Lupa Save Tadi! Aseli Hilap!

--- model/orders_detail.js
+++ model/orders_detail.js
@@ -1,32 +1,35 @@
-const Sequelize = require('sequelize');
-const sequelizeConfig = require('../config/sequelize');
+const Sequelize = require("sequelize");
+const sequelizeConfig = require("../config/sequelize");
 
-class model extends Sequelize.Model { }
-model.init({
+class model extends Sequelize.Model {}
+model.init(
+  {
     id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     order_id: Sequelize.INTEGER,
     buku_id: Sequelize.INTEGER,
-    title:Sequelize.STRING,
+    title: Sequelize.STRING,
     quantity: Sequelize.INTEGER,
-    harga:Sequelize.INTEGER,
+    harga: Sequelize.INTEGER,
     total: Sequelize.INTEGER,
     created_at: Sequelize.DATE,
     updated_at: Sequelize.DATE,
     deleted_at: Sequelize.DATE,
-}, {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    deletedAt: 'deleted_at',
+  },
+  {
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    deletedAt: "deleted_at",
     freezeTableName: true,
     timestamps: true,
     paranoid: true,
     sequelize: sequelizeConfig,
-    modelName: 'orders_detail',
-    tableName: 'orders_detail',
-});
+    modelName: "orders_detail",
+    tableName: "orders_detail",
+  }
+);
 
 module.exports = model;
